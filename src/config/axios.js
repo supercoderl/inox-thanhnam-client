9ecc4119557c0bb02a7dcf5109e037660079bc5b
supercoderl <150410325+supercoderl@users.noreply.github.com>
2024-03-29 17:21:19 +0700

    deploy

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5112/api/',
+    baseURL: 'https://inox.somee.com/api/',
 });
 
 axiosInstance.interceptors.request.use(
